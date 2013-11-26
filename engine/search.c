4e26e269e1f6eabf54bd4eac3088a014626e330e
Gustaf Ullberg <gustaf.ullberg@gmail.com>
2013-11-26 14:05:19 +0100

    Minor search refactoring

--- engine/search.c
+++ engine/search.c
@@ -29,34 +29,25 @@ static int minimax_min(chess_state_t *s1, move_t *stack, int depth, move_t *move
     int checkmate;
     chess_state_t s2;
 
+    if(depth <= 0) {
+        return state_evaluate(s1);
+    }
+
     num_moves = state_generate_moves(s1, stack, &checkmate);
     if(checkmate) {
         return INT_MIN;
     }
-    if(depth <= 1) {
-        for(i = 0; i < num_moves; i++) {
-            state_clone(&s2, s1);
-            state_apply_move(&s2, stack[i]);
-            score = state_evaluate(&s2);
-            if(score < min_score) {
-                min_score = score;
-                *move = stack[i];
-            }
+
+    for(i = 0; i < num_moves; i++) {
+        state_clone(&s2, s1);
+        state_apply_move(&s2, stack[i]);
+        score = minimax_max(&s2, &stack[num_moves], depth-1, &next_move);
+        if(score < min_score) {
+            min_score = score;
+            *move = stack[i];
         }
-        return min_score;
-        
-    } else {
-        for(i = 0; i < num_moves; i++) {
-            state_clone(&s2, s1);
-            state_apply_move(&s2, stack[i]);
-            score = minimax_max(&s2, &stack[num_moves], depth-1, &next_move);
-            if(score < min_score) {
-                min_score = score;
-                *move = stack[i];
-            }
-        }
-        return min_score;
     }
+    return min_score;
 }
 
 static int minimax_max(chess_state_t *s1, move_t *stack, int depth, move_t *move)
@@ -69,34 +60,25 @@ static int minimax_max(chess_state_t *s1, move_t *stack, int depth, move_t *move
     int checkmate;
     chess_state_t s2;
 
+    if(depth <= 0) {
+        return state_evaluate(s1);
+    }
+    
     num_moves = state_generate_moves(s1, stack, &checkmate);
     if(checkmate) {
         return INT_MAX;
     }
-    if(depth <= 1) {
-        for(i = 0; i < num_moves; i++) {
-            state_clone(&s2, s1);
-            state_apply_move(&s2, stack[i]);
-            score = state_evaluate(&s2);
-            if(score > max_score) {
-                max_score = score;
-                *move = stack[i];
-            }
+    
+    for(i = 0; i < num_moves; i++) {
+        state_clone(&s2, s1);
+        state_apply_move(&s2, stack[i]);
+        score = minimax_min(&s2, &stack[num_moves], depth-1, &next_move);
+        if(score > max_score) {
+            max_score = score;
+            *move = stack[i];
         }
-        return max_score;
-        
-    } else {
-        for(i = 0; i < num_moves; i++) {
-            state_clone(&s2, s1);
-            state_apply_move(&s2, stack[i]);
-            score = minimax_min(&s2, &stack[num_moves], depth-1, &next_move);
-            if(score > max_score) {
-                max_score = score;
-                *move = stack[i];
-            }
-        }
-        return max_score;
     }
+    return max_score;
 }
 
 int search_is_check(chess_state_t *state, move_t *stack)
