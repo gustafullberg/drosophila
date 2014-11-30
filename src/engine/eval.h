9de8966d66d286d54d595415555d29e30dfb0633
Gustaf Ullberg <gustaf.ullberg@gmail.com>
2014-11-30 09:05:39 +0100

    Commented out unused functions

--- src/engine/eval.h
+++ src/engine/eval.h
@@ -10,8 +10,8 @@ extern const short piece_value[NUM_TYPES];
 short EVAL_evaluate_board(const chess_state_t *s);
 int   EVAL_position_is_attacked(const chess_state_t *s, const int color, const int pos);
 short EVAL_material_midgame(const chess_state_t *s);
-short EVAL_pawn_structure(const chess_state_t *s);
-short EVAL_get_pawn_score(const chess_state_t *s, hashtable_t *hashtable);
+//short EVAL_pawn_structure(const chess_state_t *s);
+//short EVAL_get_pawn_score(const chess_state_t *s, hashtable_t *hashtable);
 
 static inline short EVAL_get_piecesquare(const int color, const int type, const int pos)
 {
