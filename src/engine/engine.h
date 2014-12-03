82c36cfbb0cdab40b20b8f0bc2ab39f77a3c5703
Gustaf Ullberg <gustaf.ullberg@gmail.com>
2014-12-03 09:20:18 +0100

    Prepared for asynchronous thinking

--- src/engine/engine.h
+++ src/engine/engine.h
@@ -13,6 +13,9 @@
 #define ENGINE_PROMOTION_ROOK       3
 #define ENGINE_PROMOTION_QUEEN      4
 
+#define ENGINE_SEARCH_DONE          0
+#define ENGINE_SEARCH_RUNNING       1
+
 typedef struct engine_state engine_state_t;
 typedef void (*thinking_output_cb)(int ply, int score, int time_ms, unsigned int nodes, int pv_length, int *pos_from, int *pos_to, int *promotion_type);
 
@@ -22,6 +25,10 @@ void ENGINE_reset(engine_state_t *state);
 int  ENGINE_apply_move(engine_state_t *state, const int pos_from, const int pos_to, const int promotion_type);
 int  ENGINE_apply_move_san(engine_state_t *state, const char *san);
 void ENGINE_think(engine_state_t *state, const int moves_left_in_period, const int time_left_ms, const int time_incremental_ms, int *pos_from, int *pos_to, int *promotion_type, const unsigned char max_depth);
+void ENGINE_think_start(engine_state_t *state, const int moves_left_in_period, const int time_left_ms, const int time_incremental_ms, const unsigned char max_depth);
+void ENGINE_think_stop(engine_state_t *state);
+int  ENGINE_think_get_status(engine_state_t *state);
+void ENGINE_think_get_result(engine_state_t *state, int *pos_from, int *pos_to, int *promotion_type);
 int  ENGINE_result(const engine_state_t *state);
 void ENGINE_register_thinking_output_cb(engine_state_t *state, thinking_output_cb think_cb);
 void ENGINE_resize_hashtable(engine_state_t *state, const int size_mb);
