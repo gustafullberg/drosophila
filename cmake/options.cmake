a6e913ddf8c9af59083f9fac7a6d6fc0e7ccc6da
Gustaf Ullberg <gustaf.ullberg@gmail.com>
2018-01-08 19:33:52 +0100

    CMake cleaning

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,10 +1,5 @@
 option(BUILD_EXECUTABLE "Build executable with Xboard interface" ON)
 option(BUILD_TESTS "Build test binaries" OFF)
-option(USE_PAWN_STRUCTURE "Use pawn structure assessment" OFF)
-
-if(USE_PAWN_STRUCTURE)
-add_definitions(-DPAWN_STRUCTURE)
-endif()
 
 if(BUILD_EXECUTABLE)
 set(TARGET_SUFFIX "" CACHE STRING "String to append to name of executables")
