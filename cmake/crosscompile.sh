51916ee06c97b78fe1145f2fb463f19a4935d4a1
Gustaf Ullberg <gustaf.ullberg@gmail.com>
2018-03-14 07:49:14 -0700

    Remove 32-bit builds from releases

--- cmake/crosscompile.sh
+++ cmake/crosscompile.sh
@@ -14,21 +14,9 @@ cmake ../.. -DTARGET_SUFFIX="win64" -DCMAKE_TOOLCHAIN_FILE=../../cmake/win64.cma
 make install
 cd ..
 
-mkdir -p win32
-cd win32
-cmake ../.. -DTARGET_SUFFIX="win32" -DCMAKE_TOOLCHAIN_FILE=../../cmake/win32.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../${INSTALL_DIR}
-make install
-cd ..
-
 mkdir -p linux64
 cd linux64
 cmake ../.. -DTARGET_SUFFIX="linux64" -DCMAKE_C_FLAGS="-m64 -mpopcnt" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../${INSTALL_DIR}
-make install
-cd ..
-
-mkdir -p linux32
-cd linux32
-cmake ../.. -DTARGET_SUFFIX="linux32" -DCMAKE_C_FLAGS="-m32" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../${INSTALL_DIR}
 make install
 cd ..
 
