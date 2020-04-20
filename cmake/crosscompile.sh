b4e14766ce36ba951e80d881ad18d931686c8698
Gustaf Ullberg <gustaf.ullberg@gmail.com>
2020-04-20 14:08:46 +0200

    Use LZCNT in release builds

--- cmake/crosscompile.sh
+++ cmake/crosscompile.sh
@@ -10,13 +10,13 @@ mkdir binaries
 
 mkdir -p win64
 cd win64
-cmake ../.. -DTARGET_SUFFIX="win64" -DCMAKE_TOOLCHAIN_FILE=../../cmake/win64.cmake -DCMAKE_C_FLAGS="-mpopcnt" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../${INSTALL_DIR}
+cmake ../.. -DTARGET_SUFFIX="win64" -DCMAKE_TOOLCHAIN_FILE=../../cmake/win64.cmake -DCMAKE_C_FLAGS="-mpopcnt -mlzcnt" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../${INSTALL_DIR}
 make install
 cd ..
 
 mkdir -p linux64
 cd linux64
-cmake ../.. -DTARGET_SUFFIX="linux64" -DCMAKE_C_FLAGS="-m64 -mpopcnt" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../${INSTALL_DIR}
+cmake ../.. -DTARGET_SUFFIX="linux64" -DCMAKE_C_FLAGS="-m64 -mpopcnt -mlzcnt" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../${INSTALL_DIR}
 make install
 cd ..
 
